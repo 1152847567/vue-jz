f545aba1e6aea62c34e66f07ea5d7bb27dd9420b
1152847567 <46396370+1152847567@users.noreply.github.com>
2019-05-07 15:58:28 +0800

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,67 @@
+<template>
+<!-- 子组件的模板 -->
+  <div class="app">
+    <transition>
+      <router-view></router-view>
+    </transition>
+
+    <van-tabbar v-model="active">
+      <van-tabbar-item icon="wap-home" to="/home">首页</van-tabbar-item>
+      <van-tabbar-item icon="balance-list-o" to="/budget">预算</van-tabbar-item>
+      <!-- <van-tabbar-item icon="add" to="/bookkeeping">记账</van-tabbar-item>
+      <van-tabbar-item icon="balance-o" to="/deposit">存钱</van-tabbar-item>
+      <van-tabbar-item icon="user-o" to="/mine">我的</van-tabbar-item> -->
+    </van-tabbar>
+  </div>
+</template>
+<script>
+// es6 的暴露方式 类似于 module.export = {}
+  export default {
+    data: ()=>({
+      active: 0,
+    }),
+    methods: {
+      goBack(){
+        this.$router.go(-1);
+      } 
+    }
+  }
+
+  
+</script>
+
+
+<style lang="less">
+.app {
+  // overflow-x: hidden;
+  margin-bottom: 50px;
+  .v-enter {
+    opacity: 0;
+    transform: translateX(100%);
+  }
+  .v-leave-to {
+    transform: translateX(-200%);
+    position: absolute;
+  }
+  .v-enter-active,
+  .v-leave-active {
+    transition: all 0.5s ease;
+  }
+  .van-tabbar-item:nth-child(3) {
+    .van-tabbar-item__icon {
+      width: 44px;
+      height: 44px;
+      margin: -25px 0 5px 7px;
+      color: #3CB5FF;
+      z-index: 9999;
+      i {
+        font-size: 44px;
+      }
+    }
+  }
+  .van-tabbar-item--active {
+    color: #3CB5FF;
+  }
+}
+
+</style>
---
+++ main.js
@@ -0,0 +1,45 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import router from './router'
+import App from './App'
+// 引入vant UI
+import {NavBar,Tabbar,TabbarItem,Icon,Picker,Popup,Toast,Area } from 'vant';
+Vue.use(NavBar).use(Tabbar).use(TabbarItem).use(Icon).use(Picker).use(Popup).use(Toast).use(Area);
+// import 'vant/lib/icon/local.css';
+
+// 引入store
+import store from './store'
+
+// 引入echarts
+import echarts from 'echarts'
+Vue.prototype.$echarts = echarts 
+
+// 引入vue-resource 发送ajax
+import VueResource from 'vue-resource';
+Vue.use(VueResource);
+// 配置全局的跟路径
+Vue.http.options.root = 'http://192.168.0.140:8088/'
+Vue.http.options.emulateJSON = true;
+
+// 引入地址三级联动插件
+import Distpicker from 'v-distpicker'
+Vue.component('v-distpicker', Distpicker)
+
+
+// 定义全局时间过滤器
+// import moment from 'moment'
+// Vue.filter('dataFormat', function (datastr, pattern = "YYYY-MM-DD HH:mm:ss") {
+//   return moment(datastr).format(pattern);
+// })
+
+// vue底层的性能优化
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  store,
+  render: c => c(App)
+})
